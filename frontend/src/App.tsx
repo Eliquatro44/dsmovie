e641e83363c50aa748d997b7fe3a2ac6319e17ae
Lucas Araujo <araujo.l4k@hotmail.com>
2024-09-16 11:35:39 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
-import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Listing from 'pages/Listing';
+import Form from 'pages/Form';
+import Navbar from "components/Navbar";
 
 function App() {
   return (
-    <Navbar />
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form" element={<Form />} />
+      </Routes>
+    </Router>
   );
 }
 
