c3e143e55d9ac34c53168d2aadcedc2778eaba9d
Lucas Araujo <araujo.l4k@hotmail.com>
2024-09-17 20:47:48 -0300

    Domain model, database seed (Falta a tabela score)

---
+++ backend/src/main/java/com/devsuperior3/dsmovie3/entities/Score.java
@@ -0,0 +1,42 @@
+package com.devsuperior3.dsmovie3.entities;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_score")
+public class Score {
+
+    @EmbeddedId
+    private ScorePK id = new ScorePK();
+    private Double value;
+
+    public Score() {
+    }
+
+    // Getters and Setters
+    public void setMovie(Movie movie) {
+        id.setMovie(movie);
+    }
+
+    public void setUser(User user) {
+        id.setUser(user);
+    }
+
+    public ScorePK getId() {
+        return id;
+    }
+
+    public void setId(ScorePK id) {
+        this.id = id;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+}
