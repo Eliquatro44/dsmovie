5e2e82a30f12ddb6a1c1c9006bd08e8525c70fcc
Lucas Araujo <araujo.l4k@hotmail.com>
2024-09-18 14:57:42 -0300

    Find movies (Error2 com frontcompleto)

--- backend/src/main/java/com/devsuperior3/dsmovie3/controllers/MovieControleller.java
+++ backend/src/main/java/com/devsuperior3/dsmovie3/controllers/MovieControleller.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,4 +23,8 @@ public class MovieControleller {
 		return service.findAll(peageable);
 	}
 
+	@GetMapping(value = "/{id}")
+	public MovieDTO findById(@PathVariable Long id) {
+		return service.findById(id);
+	}
 }
